7755f0a43dce7a4af52f955c7b6304f9a13d4f10
Louis Nguyen <louisn@LouisN-MacBook-Pro.local>
2021-05-02 19:29:09 -0500

    first about section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-document.addEventListener('click', () => {
-    $( "main" ).load( "about.html main" ); 
+
+document.getElementById("nav-item-home").addEventListener('click', () => {
+    $( "main" ).load( "index.html main" ); 
 });
+document.getElementById("nav-item-about").addEventListener('click', () => {
+    $( "main" ).load( "about.html main" ); 
+
+});
+document.getElementById("nav-item-contact").addEventListener('click', () => {
+    $( "main" ).load( "contact.html main" ); 
+});
+
+
