1e90c7e08c5eaf8cbbf3db5df1026a4e19044a7b
Louis Nguyen <louishnguyen01@gmail.com>
2021-05-14 22:05:18 -0500

    animations

--- js/main.js
+++ js/main.js
@@ -1,46 +1,54 @@
 
 document.getElementById("nav-item-home").addEventListener('click', () => {
+  $("main").empty();
+  $("main").load("index.html main");
+  //Sets timer for animation
+  window.setTimeout(function () {
+    $(".loader-wrapper").fadeOut("slow");
+  }, 500);
+  $("body").css("background-image", "url('img/pexels-sam-kolder-2387873.webp')");
 
-    $( "main" ).load( "index.html main" ); 
-    $("main").css("background-image", "none");
-    $("body").css("background-image", "none");
-    $("body").css("background-image", "url('img/pexels-sam-kolder-2387873.webp')");
-    $("body").css("background-image", "no-repeat");
-    $("body").css("background-size", "cover");
 });
 document.getElementById("nav-item-about").addEventListener('click', () => {
-    $( "main" ).load( "about.html main" ); 
-    $("main").css("background-image", "none");
-    $("body").css("background-image", "none");
-    $("body").css("background-image", "url('img/pexels-aarti-vijay-2693529.webp')");
+  //clears and loads main content
+  $("main").empty();
+  $("main").load("about.html main");
+  //Sets timer for animation
+  window.setTimeout(function () {
+    $(".loader-wrapper").fadeOut("slow");
+  }, 1000);
+  //serves background img
+  $("body").css("background-image", "url('img/pexels-aarti-vijay-2693529.webp')");
 });
 document.getElementById("nav-item-contact").addEventListener('click', () => {
-    $( "main" ).load( "contact.html main" ); 
-    $("main").css("background-image", "none");
-    $("body").css("background-image", "none");
-    $("body").css("background-image", "url('img/pexels-sanaan-mazhar-3075993.webp')");
+  $("main").empty();
+  $("main").load("contact.html main");
+  window.setTimeout(function () {
+    $(".loader-wrapper").fadeOut("slow");
+  }, 1000);
+
+  $("body").css("background-image", "url('img/pexels-sanaan-mazhar-3075993.webp')");
 });
 
-    var form = document.getElementById("my-form");
-    
-    async function handleSubmit(event) {
-      event.preventDefault();
-      var status = document.getElementById("my-form-status");
-      var data = new FormData(event.target);
-      fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-      }).then(response => {
-        status.innerHTML = "Thanks for your submission!";
-        form.reset()
-      }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form"
-      });
+var form = document.getElementById("my-form");
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      'Accept': 'application/json'
     }
-    form.addEventListener("submit", handleSubmit)
+  }).then(response => {
+    status.innerHTML = "Thanks for your submission!";
+    form.reset()
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
 
 
-    
+
